55854562d0cc2962d70aa4053a35b22ef0bd34b9
DanilBond <75364237+DanilBond@users.noreply.github.com>
2021-01-06 06:18:32 +0600

    Now sound works only after 10 am

--- getPlayersCountOnServer.js
+++ getPlayersCountOnServer.js
@@ -3,6 +3,8 @@ var players = "";
 var playersCount = 0;
 var button = document.getElementsByClassName("btn btn-success btn-block");
 var audio = new Audio('https://mp3melodii.ru/files_site_02/001/mellen_gi_remix_na_budilnik.mp3');
+var date = new Date();
+var currentHour = 0;
 
 setTmou();
 function setTmou(){
@@ -10,16 +12,21 @@ function setTmou(){
 }
 
 function CheckPlayers() {
- button[0].click();
-console.log(playerText[0].innerHTML);
+button[0].click();
 players = playerText[0].innerHTML;
 players = players[0] + players[1];
-console.log(players);
 playersCount = parseInt(players);
 console.log(playersCount);
+console.log(date.getHours() + ":" + date.getMinutes());	
 if(playersCount <= 20){
+	currentHour = date.getHours();
+	if(currentHour >= 10){
 	console.log("Raidss!!!");
 	audio.play();
+	}
+	else{
+	console.log("Waiting a sun");	
+	}
 }
 else{
 	console.log("I need some sleep");
